6a7ec367e3ae82f33fe0b93d44bec4968e16b4af
KhushalP0126 <67284540+KhushalP0126@users.noreply.github.com>
2025-12-03 17:29:32 -0500

    Fix HEX display mapping for cpu_gpio_out
    Reordered hexdriver instances to match the correct nibble mapping from cpu_gpio_out.

--- top.sv
+++ top.sv
@@ -60,15 +60,15 @@ module top (
     // ---------------------------------------------------------------------
     // If your hexdriver module has different port names, adapt the instance ports.
     // Here each hexdriver instance converts a 4-bit nibble to a 7-bit segment vector.
-    // HEX0 shows the ones digit (bits [31:28]) so the number reads HEX0..HEX7
-    hexdriver hd0  (.in(cpu_gpio_out[31:28]), .out(HEX0));
-    hexdriver hd1  (.in(cpu_gpio_out[27:24]), .out(HEX1));
-    hexdriver hd2  (.in(cpu_gpio_out[23:20]), .out(HEX2));
-    hexdriver hd3  (.in(cpu_gpio_out[19:16]), .out(HEX3));
-    hexdriver hd4  (.in(cpu_gpio_out[15:12]), .out(HEX4));
-    hexdriver hd5  (.in(cpu_gpio_out[11: 8]), .out(HEX5));
-    hexdriver hd6  (.in(cpu_gpio_out[ 7: 4]), .out(HEX6));
-    hexdriver hd7  (.in(cpu_gpio_out[ 3: 0]), .out(HEX7));
+    // Map nibble 0 -> HEX0 (ones) up through nibble 7 -> HEX7 (MSD)
+    hexdriver hd0  (.in(cpu_gpio_out[ 3: 0]), .out(HEX0));
+    hexdriver hd1  (.in(cpu_gpio_out[ 7: 4]), .out(HEX1));
+    hexdriver hd2  (.in(cpu_gpio_out[11: 8]), .out(HEX2));
+    hexdriver hd3  (.in(cpu_gpio_out[15:12]), .out(HEX3));
+    hexdriver hd4  (.in(cpu_gpio_out[19:16]), .out(HEX4));
+    hexdriver hd5  (.in(cpu_gpio_out[23:20]), .out(HEX5));
+    hexdriver hd6  (.in(cpu_gpio_out[27:24]), .out(HEX6));
+    hexdriver hd7  (.in(cpu_gpio_out[31:28]), .out(HEX7));
 
     // ---------------------------------------------------------------------
     // Optional: tie unused LEDs off (or hook to other signals you like)
